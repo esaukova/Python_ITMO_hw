bbc9f2b7407ee28b5708b2552e1d28fb258b23da
esaukova <esaukova@gmail.com>
2026-01-18 21:05:10 +0300

    Good_kid_decision

---
+++ good_kid.py
@@ -0,0 +1,15 @@
+t = int(input())
+for i in range(t):
+    num_digits = int(input())
+    digits = list(map(int, input().split()))
+
+    max_value = 0
+    for digit_idx in range(num_digits):
+        modified_digits = digits.copy()
+        modified_digits[digit_idx] += 1
+        current_value = 1
+        for digit in modified_digits:
+            current_value *= digit
+        max_value = max(max_value, current_value)
+
+    print(max_value)
